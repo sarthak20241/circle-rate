2a2555f9a414a12b6d6a4ba3bee8683568b34222
sarthak20241 <sarthak20241@iiitd.ac.in>
2025-07-04 18:03:08 +0530

    initializing repository

---
+++ src/main/java/com/circlerate/circle_rate/CircleRateApplication.java
@@ -0,0 +1,14 @@
+package com.circlerate.circle_rate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleRateApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		SpringApplication.run(CircleRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/circlerate/circle_rate/CircleRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.circlerate.circle_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
